5b355f61a02230440f73c04ed56155cebccf91c1
Nikolaj Ivancic <nik@congral.com>
2017-03-13 16:20:26 -0400

    feat(all): initial version

---
+++ carmel/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Carmel.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return new ChallengeResult("Auth0", new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+
+        [Authorize]
+        public async Task Logout()
+        {
+            var redirectUri = Url.Action("Index", "App");
+            await HttpContext.Authentication.SignOutAsync("Auth0", new AuthenticationProperties
+            {
+                // Indicate here where Auth0 should redirect the user after a logout.
+                // Note that the resulting absolute Uri must be whitelisted in the 
+                // **Allowed Logout URLs** settings for the client.
+                RedirectUri = redirectUri
+            });
+            await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        /// <summary>
+        /// This is just a helper action to enable you to easily see all claims related to a user. It helps when debugging your
+        /// application to see the in claims populated from the Auth0 ID Token
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        public IActionResult Claims()
+        {
+            return View();
+        }
+    }
+}
